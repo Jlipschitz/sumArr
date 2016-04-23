e18a064d613e037ce41743dbeb79eb32ed6b0547
Jamie Lipschitz <Jay.lipschitz@gmail.com>
2016-04-23 12:28:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function sumAll(arr) {
   var longNum = Math.max.apply(null, arr);
   var shortNum = Math.min.apply(null, arr);
   
+  //store values in temp array and return temp total using reduce()
   for(var i = shortNum ; i <= longNum; i++) {
       temp.push(i);
   } 
